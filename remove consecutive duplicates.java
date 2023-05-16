fd8915383a71f02541400817765dc25b29e5a429
Siddh Srivastava <118525926+Siddh-s@users.noreply.github.com>
2023-05-17 01:07:01 +0530

    Create remove consecutive duplicates.java

---
+++ remove consecutive duplicates.java
@@ -0,0 +1,23 @@
+import  java.util.ArrayList;
+public class removeconsecutiveduplicates {
+    public static ArrayList<Integer> removeConsecutiveDuplicates(int arr[]){
+        
+       ArrayList<Integer> result = new ArrayList<>();
+       result.add(arr[0]);
+       for(int i = 1;i<arr.length;i++)
+       {
+        if(arr[i]!= arr[i-1]){
+            result.add(arr[i]);
+        }
+       } 
+       return result;
+    }
+    public static void main (String args[]){
+        int arr[]= {10,10,20,20,20,40,70};
+        ArrayList<Integer> result = removeConsecutiveDuplicates(arr);
+        for(int i = 0 ; i<result.size();i++){
+            System.out.println(result.get(i));
+        }
+
+    }
+}
