d0733f1744594ba0d71d4922b8990d24cff3d38d
Siddh Srivastava <118525926+Siddh-s@users.noreply.github.com>
2023-05-18 21:35:51 +0530

    Create length of linkedlist.java

---
+++ length of linkedlist.java
@@ -0,0 +1,46 @@
+package germany;
+
+public class node<T> {
+    T data;
+    node<T> next;
+    node(T data){
+        this.data = data;
+    }
+    
+}
+public class linkedlistUse {
+    public static node<Integer> createLinkedlist(){
+       node<Integer> n1 = new node(10);
+       node<Integer> n2 = new node(20);
+       node<Integer> n3 = new node(30);
+       node<Integer> n4 = new node(40);
+       n1.next = n2;
+       n2.next = n3;
+       n3.next = n4;
+       return n1;
+    }
+ public static void prints (node<Integer> head){
+    while(head!= null){
+        System.out.print (head.data + " ");
+        head = head.next;
+    }
+    System.out.println();
+ }   
+ public static int length (node<Integer> head){
+    int count = 0;
+    node<Integer> temp = head ;
+    while(temp!= null){
+        count++;
+        temp = temp.next;
+    }
+    return count;
+ }   
+
+    public static void main (String args[]){
+        node<Integer> head = createLinkedlist();
+        prints(head);
+        System.out.println(length(head));
+       
+        
+    }
+}
